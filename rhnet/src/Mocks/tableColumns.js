f1f34663fedfb427fa2eeace88cac219e2d6ba75
Noa <noa.gratacos@orange.fr>
2024-07-15 11:45:18 +0200

    Add Mocks files

---
+++ rhnet/src/Mocks/tableColumns.js
@@ -0,0 +1,38 @@
+export const columnsTitles = [
+  {
+    Header: "First Name",
+    accessor: "firstName",
+  },
+  {
+    Header: "Last Name",
+    accessor: "lastName",
+  },
+  {
+    Header: "Date of Birth",
+    accessor: "birthDate",
+  },
+  {
+    Header: "Start Date",
+    accessor: "startDate",
+  },
+  {
+    Header: "Street",
+    accessor: "street",
+  },
+  {
+    Header: "City",
+    accessor: "city",
+  },
+  {
+    Header: "State",
+    accessor: "state",
+  },
+  {
+    Header: "Zip Code",
+    accessor: "zip",
+  },
+  {
+    Header: "Department",
+    accessor: "department",
+  },
+];
